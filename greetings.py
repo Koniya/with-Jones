24a6de0014921fbbde044e8a02a9f3030969ddaf
koniya <gctcml@gmail.com>
2024-01-16 14:15:42 +0800

    added types

--- greetings.py
+++ greetings.py
@@ -1,4 +1,5 @@
+"""
 print ("What is your name?")
 name = input()
 print(f"Hello {name}, welcome to our class with Jones!") # f string loads the string; short cut for string formatting
-
+"""
---
+++ types.py
@@ -0,0 +1,10 @@
+country = "Philippines"
+capital = "Manila"
+president = "BBM"
+num_cities = 70
+has_earthquake = True
+life_expectancy= 72.12
+
+mystring = f"My name is Connie. I came from the {country}. Our capital city is {capital}. Our president is {president}. My country has {num_cities} cities. It is {has_earthquake} that our country has earthquakes. The life expectancy in {country} is {life_expectancy}."
+
+print(mystring)
